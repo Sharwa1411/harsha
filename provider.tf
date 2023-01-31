c88cc04816a1b5882d5160393aa99d16b9dac39e
Venkata Nagalakshmi GayatriSarvani <vrangavajhala7418@conestogac.on.ca>
2023-01-31 12:41:24 -0500

    3 files commited

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "09a58f19-f5c2-46ac-bd5c-66478afbb057"
+  client_id       = "66ac83d4-e96e-4628-a7af-1229c7f42cda"
+  client_secret   = "5bL8Q~GP.6LCjyU2kpt1qRmzK0nBbyktDt6escBw"
+  tenant_id       = "68d4ae05-23b3-4d67-af8f-b03cc95df281"
+}
